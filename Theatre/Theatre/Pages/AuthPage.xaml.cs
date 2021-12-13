1a0ac98eae856c0157828ee3d71f6fd11d83fa02
MarkTOPI <skorikroma1981@mail.ru>
2021-12-13 20:59:27 +0600

    add Coursework

---
+++ Theatre/Theatre/Pages/AuthPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Theatre.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAuth_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.ButtonPage());
+        }
+
+        private void btnReg_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.RegistrationPage());
+        }
+    }
+}
---
+++ Theatre/Theatre/Pages/ButtonPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Theatre.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ButtonPage.xaml
+    /// </summary>
+    public partial class ButtonPage : Page
+    {
+        public ButtonPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnPersonal_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.PersonalAccountingPage());
+        }
+
+        private void BtnRepertoire_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.RepertoirePage());
+        }
+    }
+}
